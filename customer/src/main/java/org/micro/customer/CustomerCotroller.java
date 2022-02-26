93fde1a0e74eea3b55a33b0a04202cd733133674
Климович Елена Сергеевна <eklimovich@systematica-consulting.ru>
2022-02-26 19:05:37 +0300

    microservice with rest controller & connection to pg database

---
+++ customer/src/main/java/org/micro/customer/CustomerCotroller.java
@@ -0,0 +1,19 @@
+package org.micro.customer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("api/v1/customers")
+public record CustomerCotroller(CustomerService service) {
+
+    @PostMapping
+    public void registerCustomer(@RequestBody CustomerRegistrationRequest customerRequest) {
+        log.info("New Customer registration {} ", customerRequest);
+        service.register(customerRequest);
+    }
+}
---
+++ customer/src/main/java/org/micro/customer/CustomerService.java
@@ -0,0 +1,15 @@
+package org.micro.customer;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public record CustomerService(CustomerRepository repository) {
+    public void register(CustomerRegistrationRequest request) {
+        Customer customer = Customer.builder()
+                .firstName(request.firstName())
+                .lastName(request.lastName())
+                .email(request.email())
+                .build();
+        repository.save(customer);
+    }
+}
